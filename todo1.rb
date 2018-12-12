1d48d35f4310677636e590dc079d1666c8946bbf
tprax <tprax01@gmail.com>
2018-12-12 11:15:23 -0700

    create options section

--- todo1.rb
+++ todo1.rb
@@ -10,14 +10,31 @@
 #exit program
 
 def greet
-  puts "Hi Welcome to your Todo List"
+  puts "Hi Welcome to your Todo List!"
   puts "1.) Enter Todo list"
   puts "2.) Exit Todo list"
   @answer = gets.to_i
+  greet_action
+end
+
+def greet_action
+  case @answer
+  when 1
+    puts "Great lets get you started!"
+    options
+  when 2
+    puts "Okay have a great day!"
+    exit
+  end
 end
 
 def options
-
+  puts "What would you like to do today?"
+  puts "1.) Create Todo"
+  puts "2.) Delete Todo"
+  puts "3.) Complete Todo"
+  puts "4.) Exit"
+  @answer2 = gets.to_i
 end
 
 def create_todo
