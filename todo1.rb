30aa6b2c074305b454a0050b2b5ff4b261a9f1d9
tprax <tprax01@gmail.com>
2018-12-12 10:51:14 -0700

    created greeting

---
+++ todo1.rb
@@ -0,0 +1,35 @@
+#sudo code
+#greet
+#give option to enter todo list
+#perform actions
+#give options on what they want to do
+#perform actions
+#create todo
+#delete todo
+#complete todo
+#exit program
+
+def greet
+  puts "Hi Welcome to your Todo List"
+  puts "1.) Enter Todo list"
+  puts "2.) Exit Todo list"
+  @answer = gets.to_i
+end
+
+def options
+
+end
+
+def create_todo
+  
+end
+
+def delete_todo
+
+end
+
+def complete_todo
+
+end
+
+greet
